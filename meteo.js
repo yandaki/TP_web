1baa10ce3778873ee0b36d3b4b2784c9ebde867b
yandaki <78733984+yandaki@users.noreply.github.com>
2021-02-09 02:59:32 +0100

    Add files via upload

--- meteo.js
+++ meteo.js
@@ -1,9 +1,9 @@
-let villeCourante="lyon";
 function showCity(){
 let cities = document.getElementsByClassName("city");
 let l=cities.length;
-
+let villeCourante=document.getElementsById("choix").value;
 for(let i=0; i<l; i++){
 	if(cities[i].id==villeCourante) cities[i].style.display="";
 	else cities[i].style.display="none";
+}
 }
