28ae315f55dad7cf2b6894566ce60af5f1fa1610
yandaki <78733984+yandaki@users.noreply.github.com>
2021-02-09 22:01:47 +0100

    Update meteo.js

--- meteo.js
+++ meteo.js
@@ -3,8 +3,11 @@ function showCity(){
 
 	let l=cities.length;
 	let villeCourante=document.getElementById("choix").value;
+	cities[0].style.display="none";
+	cities[1].style.display="none";
+	cities[2].style.display="none";
 	for(let i=0; i<l; i++){
-		if(cities[i].id==villeCourante) cities[i].style.display="none";
-		else cities[i].style.display="";
+		if(cities[i].id==villeCourante) cities[i].style.display="";
+		else cities[i].style.display="none";
 	}
 }
