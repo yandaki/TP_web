37ff5206ac8eb0717d413ca1a9270803c2da2c52
yandaki <78733984+yandaki@users.noreply.github.com>
2021-02-09 03:03:14 +0100

    Update meteo.js

--- meteo.js
+++ meteo.js
@@ -1,7 +1,11 @@
 function showCity(){
 let cities = document.getElementsByClassName("city");
 let l=cities.length;
+<<<<<<< Updated upstream
 let villeCourante=document.getElementById("choix").value;
+=======
+let villeCourante=document.getElementsById("choix").value;
+>>>>>>> Stashed changes
 for(let i=0; i<l; i++){
 	if(cities[i].id==villeCourante) cities[i].style.display="";
 	else cities[i].style.display="none";
